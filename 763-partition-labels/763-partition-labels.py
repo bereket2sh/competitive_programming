bed4095bc3cfb069f015b0ff9f1cf31f5881e0d2
bereket2sh <94742072+bereket2sh@users.noreply.github.com>
2022-03-22 14:33:30 +0300

    Time: 60 ms (48.46%), Space: 13.8 MB (78.52%) - LeetHub

--- 763-partition-labels/763-partition-labels.py
+++ 763-partition-labels/763-partition-labels.py
@@ -3,17 +3,17 @@ class Solution:
         #space , time = O(n),O(n)
         count = Counter(s)
         Set = set()
-        res, var, total = [],[],0
+        res, var, total = [],0,0
         for char in s:
             if not Set or char not in Set:
-                var.append(char)
+                var += 1
                 Set.add(char)
                 total += count[char] - 1
             else:
-                var.append(char)
+                var += 1
                 total -= 1
             if total == 0:
-                res.append(len(var))
+                res.append(var)
                 Set = set()
-                var = []
+                var = 0
         return res
