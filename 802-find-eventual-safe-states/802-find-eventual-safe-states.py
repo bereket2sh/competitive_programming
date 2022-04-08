56deabea85dd72d9f4b99165c93cb4b2c53c968f
bereket2sh <94742072+bereket2sh@users.noreply.github.com>
2022-04-08 08:07:05 +0300

    Time: 1203 ms (16.57%), Space: 21.3 MB (91.59%) - LeetHub

--- 802-find-eventual-safe-states/802-find-eventual-safe-states.py
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.py
@@ -1,24 +1,48 @@
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-        node = defaultdict(list)
-        out_degree = defaultdict(int)
-        queue = deque()
-        res = []
+        #color 0 is not vistied ,color 1 is visited(safe) and color 2 is in cylcle(check)
         
+        def dfs(num):  
+            if color[num] == 2:
+                return False
+            if color[num] == 1:
+                return True
+            color[num] = 2
+            for i in graph[num]:
+                if dfs(i) == False:
+                       return False
+            
+            color[num] = 1
+            return True
+
+        color = [0]*len(graph)
+        output = []
         for i in range(len(graph)):
-            out_degree[i] = len(graph[i])
-            if out_degree[i] == 0:
-                queue.append(i)
-            for j in graph[i]:
-                node[j].append(i)
+            if dfs(i): output.append(i)
         
-        while queue:
-            curr = queue.popleft()
-            res.append(curr)
-            for num in node[curr]:
-                out_degree[num] -= 1
-                if out_degree[num] == 0:
-                    queue.append(num)
+        return output
         
-        res.sort()
-        return res
+        
+        
+#         node = defaultdict(list)
+#         out_degree = defaultdict(int)
+#         queue = deque()
+#         res = []
+        
+#         for i in range(len(graph)):
+#             out_degree[i] = len(graph[i])
+#             if out_degree[i] == 0:
+#                 queue.append(i)
+#             for j in graph[i]:
+#                 node[j].append(i)
+        
+#         while queue:
+#             curr = queue.popleft()
+#             res.append(curr)
+#             for num in node[curr]:
+#                 out_degree[num] -= 1
+#                 if out_degree[num] == 0:
+#                     queue.append(num)
+        
+#         res.sort()
+#         return res
