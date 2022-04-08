02505f8849626fb5397c4b95238af30d3a2daf73
bereket2sh <94742072+bereket2sh@users.noreply.github.com>
2022-04-08 06:44:05 +0300

    Time: 101 ms (90.94%), Space: 17.8 MB (5.31%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.py
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -3,27 +3,88 @@ class Solution:
         graph = defaultdict(list)
         
         for course, pre in prerequisites:
-            graph[pre].append(course)
+            graph[course].append(pre)
             
-        indegree = [0]*numCourses
-        for i in graph:
-            for j in graph[i]:
-                indegree[j] += 1
-        
-        queue = deque()
-        for course in range(numCourses):
-            if indegree[course] == 0:
-                queue.append(course)
-        
         courseOrder = []
-        while queue:
-            pre = queue.popleft()
-            courseOrder.append(pre)
-            for course in graph[pre]:
-                indegree[course] -= 1
-                if indegree[course] == 0:
-                    queue.append(course)
+        visited, cycle = set(), set()
+        
+        def dfs(course):
+            if course in cycle:
+                return False
+            if course in visited:
+                return True
+            cycle.add(course)
+            for pre in graph[course]:
+                if dfs(pre) == False:
+                    return False
+                
+            cycle.remove(course)
+            visited.add(course)
+            courseOrder.append(course)
+            
+            return True
+        
+        for course in range(numCourses):
+            if dfs(course) == False:
+                return []
+        return courseOrder
+            
+            
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         graph = defaultdict(list)
+        
+#         for course, pre in prerequisites:
+#             graph[pre].append(course)
+            
+#         indegree = [0]*numCourses
+#         for i in graph:
+#             for j in graph[i]:
+#                 indegree[j] += 1
+        
+#         queue = deque()
+#         for course in range(numCourses):
+#             if indegree[course] == 0:
+#                 queue.append(course)
+        
+#         courseOrder = []
+#         while queue:
+#             pre = queue.popleft()
+#             courseOrder.append(pre)
+#             for course in graph[pre]:
+#                 indegree[course] -= 1
+#                 if indegree[course] == 0:
+#                     queue.append(course)
                     
-        if len(courseOrder) == numCourses:
-            return courseOrder
-        else: return []
+#         if len(courseOrder) == numCourses:
+#             return courseOrder
+#         else: return []
