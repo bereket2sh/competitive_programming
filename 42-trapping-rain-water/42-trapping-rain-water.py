eb3ac46beae070fa4381ef295002d8869a94ddd2
bereket2sh <94742072+bereket2sh@users.noreply.github.com>
2022-09-18 22:05:59 +0300

    Time: 277 ms (22.41%), Space: 16 MB (81.24%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -1,22 +1,42 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        maxLeft = [0]*len(height)
-        maxRight = [0]* len(height)
-        minMaxLeftRight = [0] * len(height)
         
-        for i in range(1,len(height)):
-            maxLeft[i] = max(height[i-1], maxLeft[i-1])
+        l, r = 0, len(height) - 1
+        maxL, maxR = height[l], height[r]
+        res = 0
         
-        for i in range(len(height)-2 , -1, -1):
-            maxRight[i] = max(height[i+1], maxRight[i+1])
+        while l < r:
+            if maxL < maxR:
+                l += 1
+                maxL = max(maxL, height[l])
+                res += maxL - height[l]
+            else:
+                r -=1 
+                maxR = max(maxR, height[r])
+                res += maxR - height[r]
         
-        for i in range(len(height)):
-            minMaxLeftRight[i] = min(maxLeft[i], maxRight[i])
+        return res
+        
+        
+        
+        
+#         maxLeft = [0]*len(height)
+#         maxRight = [0]* len(height)
+#         minMaxLeftRight = [0] * len(height)
+        
+#         for i in range(1,len(height)):
+#             maxLeft[i] = max(height[i-1], maxLeft[i-1])
+        
+#         for i in range(len(height)-2 , -1, -1):
+#             maxRight[i] = max(height[i+1], maxRight[i+1])
+        
+#         for i in range(len(height)):
+#             minMaxLeftRight[i] = min(maxLeft[i], maxRight[i])
             
-        ans = 0
-        for i in range(len(height)):
-            temp = minMaxLeftRight[i] - height[i]
-            if temp > 0:
-                ans += temp
+#         ans = 0
+#         for i in range(len(height)):
+#             temp = minMaxLeftRight[i] - height[i]
+#             if temp > 0:
+#                 ans += temp
                 
-        return ans
+#         return ans
