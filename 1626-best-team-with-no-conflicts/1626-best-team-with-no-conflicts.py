1fbb2849e8e09bd18b92dc272f9765e10dee23c9
bereket2sh <94742072+bereket2sh@users.noreply.github.com>
2023-02-01 11:28:04 +0300

    Time: 1714 ms (74.83%), Space: 14 MB (100.00%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.py
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.py
@@ -1,15 +1,21 @@
 class Solution:
     def bestTeamScore(self, scores: List[int], ages: List[int]) -> int:
-        new_list = sorted(zip(ages, scores)) 
-        visited = [new_list[0][1]] 
-        dp = [new_list[0][1]] 
-        ans = new_list[0][1] 
-
-        for i in range(1,len(new_list)):
-            s = new_list[i][1]
-            index =bisect_right(visited, s) 
-            curr = max(dp[:index]) + s if index else s 
-            ans = max(ans, curr)
-            visited.insert(index, s) 
-            dp.insert(index,curr)
-        return ans
+        
+        scoreAge = list(zip(scores, ages))
+        
+        scoreAge.sort()
+        
+        dp = sorted(scores)
+        
+        
+        for i in range(len(scoreAge)):
+            curScore, curAge = scoreAge[i]
+            
+            for j in range(i):
+                score, age = scoreAge[j]
+                
+                if curAge >= age:
+                    dp[i] = max(dp[i], dp[j] + curScore)
+                    
+        return max(dp)
+        
