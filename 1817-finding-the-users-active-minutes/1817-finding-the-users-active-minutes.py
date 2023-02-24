e35de3f570f1829d897a03b5017a585870f69eb3
bereket2sh <94742072+bereket2sh@users.noreply.github.com>
2023-02-24 21:49:21 +0300

    Time: 990 ms (96.40%), Space: 24.8 MB (45.45%) - LeetHub

--- 1817-finding-the-users-active-minutes/1817-finding-the-users-active-minutes.py
+++ 1817-finding-the-users-active-minutes/1817-finding-the-users-active-minutes.py
@@ -5,14 +5,9 @@ class Solution:
         for i, time in logs:
             d[i].add(time)
             
-        d2 = defaultdict(int)
-        
-        for key, value in d.items():
-            d2[len(value)] += 1
-            
         res = [0] * k
         
-        for i in range(k):
-            res[i] = d2[i + 1]
+        for key, value in d.items():
+            res[len(value) - 1] += 1
             
         return res
