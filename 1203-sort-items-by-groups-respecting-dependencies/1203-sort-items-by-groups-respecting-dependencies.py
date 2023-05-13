f5ddbe3940402ecbdafbb8545c656adabfea0a31
bereket2sh <94742072+bereket2sh@users.noreply.github.com>
2023-05-13 13:45:47 +0300

    Time: 543 ms (16.50%), Space: 41.8 MB (35.05%) - LeetHub

--- 1203-sort-items-by-groups-respecting-dependencies/1203-sort-items-by-groups-respecting-dependencies.py
+++ 1203-sort-items-by-groups-respecting-dependencies/1203-sort-items-by-groups-respecting-dependencies.py
@@ -10,10 +10,8 @@ class Solution:
                 count += 1
                 
         for val, gr in enumerate(group):
-            
             if val not in graph[gr]:
                 graph[gr][val] = [[], [0]]
-                
             if gr not in group_graph:
                 group_graph[gr] = [[], [0]]
 
@@ -35,43 +33,33 @@ class Solution:
 
                     group_graph[gr][1][0] += 1
                     
-        # print(graph)
-        # print(group_graph)
         def topological(g):
-            n = len(g.keys())
             order = []
             que = deque()
-            # print("first")
             for key in g.keys():
-                # print(key)
                 if g[key][1][0] == 0:
                     que.append(key)
                     
             while que:
                 temp = que.popleft()
                 order.append(temp)
-                n -= 1
                 
                 for child in g[temp][0]:
                     g[child][1][0] -= 1
                     if g[child][1][0] == 0:
                         que.append(child)
                         
-            if n != 0:
-                return []
-            return order
+            return order         
+            
         ans = []
-        
         var = topological(group_graph)
-        if not var:
-            return []
+        
         for i in range(len(var)):
             temp = topological(graph[var[i]])
-            if not temp: return []
             for t in temp:
                 ans.append(t)
                 
-        return ans
+        return ans if len(ans) == len(group) else []
             
             
                         
