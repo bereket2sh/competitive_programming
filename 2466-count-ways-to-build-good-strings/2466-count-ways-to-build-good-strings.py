048fb648cf3c4e8658da7d4cca1ff6e61d74a130
bereket2sh <94742072+bereket2sh@users.noreply.github.com>
2023-05-13 15:35:55 +0300

    Time: 393 ms (43.31%), Space: 156 MB (40.95%) - LeetHub

--- 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.py
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.py
@@ -1,18 +1,23 @@
 class Solution:
     def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
         self.modulo = 10 ** 9 + 7
-        @cache
+        memo = defaultdict(int)
+        
         def dp(num):
+            if num in memo:
+                return memo[num]
+            
             if num > high:
                 return 0
             if low <= num <= high:
-                val = 1
-                val += dp(num + zero) + dp(num + one)
-                return val % self.modulo
+                memo[num] = 1
+                memo[num] += dp(num + zero) + dp(num + one)
+                return memo[num] % self.modulo
             
-            ans = 0
-            ans += dp(num + zero) + dp(num + one)
+           
+            memo[num] += dp(num + zero) + dp(num + one)
+            memo[num] = memo[num] % self.modulo
             
-            return ans % self.modulo
+            return memo[num] 
         
         return dp(0)
