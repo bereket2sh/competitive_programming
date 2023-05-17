b98e0b3e84cbc208566ae5c1f540ea35e1db2c01
bereket2sh <94742072+bereket2sh@users.noreply.github.com>
2023-05-17 15:09:37 +0300

    Time: 1976 ms (62.24%), Space: 51.4 MB (96.68%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.py
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.py
@@ -1,36 +1,34 @@
 class Solution:
     def getOrder(self, tasks: List[List[int]]) -> List[int]:
         
-        
         for i in range(len(tasks)):
             tasks[i].append(i)
             
-        tasks.sort()
+        heapify(tasks)
         
         cur_time = 0
         
         ans = []
-        ind = 0
         current = []
         
-        while ind < len(tasks):
+        while tasks:
             
             
-            while ind < len(tasks):
+            while tasks:
                 
-                if tasks[ind][0] <= cur_time:
-                    heappush(current, [tasks[ind][1], tasks[ind][-1] , tasks[ind][0]])
-                    ind += 1
+                if tasks[0][0] <= cur_time:
+                    task = heappop(tasks)
+                    heappush(current, [task[1], task[-1] , task[0]])
                 else:
                     break
-            # print(current) 
+
             if current:
                 temp = heappop(current)
                 ans.append(temp[1])
                 
                 cur_time += temp[0]
             else:
-                cur_time = tasks[ind][0]
+                cur_time = tasks[0][0]
         while current:
             temp = heappop(current)
             ans.append(temp[1])
