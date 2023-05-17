a696a35f8208ae14550e6acca62f5402c1454022
bereket2sh <94742072+bereket2sh@users.noreply.github.com>
2023-05-17 14:23:07 +0300

    Time: 52 ms (17.19%), Space: 16.2 MB (22.84%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -1,22 +1,27 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        s = s.strip()
+        state = 0
        
         sign = 1
         ans = ""
         for i, n in enumerate(s):
-            if (n == "+" and i != 0) or (n == "-" and i != 0):
+            if (n == "+" and i != 0 and state != 0) or (n == "-" and i != 0 and state != 0):
                 break
-            if n == "-":
-                sign *= -1 
+            elif n == "-":
+                sign *= -1
+                state += 1
             elif n == "+":
+                state += 1
                 continue
             
             elif n.isdigit():
+                state += 1
                 ans += n
+            elif n == " " and state == 0:
+                continue
             else:
                 break
-                
+        
         if not ans: return 0
         
         ans = sign * int(ans)
