30876861d44239709d3ade1685c2949d623d10c3
bereket2sh <94742072+bereket2sh@users.noreply.github.com>
2023-05-18 18:00:11 +0300

    Time: 4057 ms (5.32%), Space: 161.1 MB (5.02%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -1,19 +1,21 @@
 class Solution:
     def minScore(self, n: int, roads: List[List[int]]) -> int:
-        graph = defaultdict(dict)
-        for u, v, w in roads:
-            graph[u][v] = graph[v][u] = w
         
-        res = inf
-        vis = set()
-        dq = deque([1])
-
-        while dq:
-            node = dq.popleft()
-            for adj, scr in graph[node].items():
-                if adj not in vis:
-                    dq.append(adj)
-                    vis.add(adj)
-                res = min(res,scr)
-                
-        return res
+        graph = defaultdict(list)
+        
+        for a, b, d in roads:
+            graph[a].append([b,d])
+            graph[b].append([a, d])
+        # print(graph)
+        self.ans = float('inf')
+        
+        def dfs(node, distance):
+            self.ans = min(self.ans, distance)
+            for child, d in graph[node]:
+                if (child, node) not in seen and (node, child) not in seen:
+                    seen.add((node, child))
+                    dfs(child, d)
+        
+        seen = set()      
+        dfs(1, float('inf'))
+        return self.ans
