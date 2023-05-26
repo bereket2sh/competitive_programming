aaa8dcf6c4b5de85a2935f8b7bdbdbe19201d279
bereket2sh <94742072+bereket2sh@users.noreply.github.com>
2023-05-26 10:43:28 +0300

    Time: 103 ms (6.83%), Space: 27.3 MB (5.09%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -1,6 +1,19 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
-        return  list(chain(*[[x] * y for x, y in sorted(Counter(s).items(), key = lambda x : -x[1])])) 
+        # you won't understand my code :)
+        # return  list(chain(*[[x] * y for x, y in sorted(Counter(s).items(), key = lambda x : -x[1])])) 
+        
+        cnt = Counter(s)
+        n = len(s)
+        bucket = [[] for _ in range(n+1)]
+        for c, freq in cnt.items():
+            bucket[freq].append(c)
+        
+        ans = []
+        for freq in range(n, -1, -1):
+            for char in bucket[freq]:
+                ans.extend([char] * freq)
+        return "".join(ans)
         
             
         
