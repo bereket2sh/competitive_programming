6b9f47640e303951d02d7da1257ca04211ec8ea3
bereket2sh <94742072+bereket2sh@users.noreply.github.com>
2023-06-01 17:08:02 +0300

    Time: 5207 ms (40.34%), Space: 17 MB (80.67%) - LeetHub

--- 2076-process-restricted-friend-requests/2076-process-restricted-friend-requests.py
+++ 2076-process-restricted-friend-requests/2076-process-restricted-friend-requests.py
@@ -1,65 +1,23 @@
-class parent(defaultdict):
-    def __missing__(self, key):
-        return key
-class UnionFind:
-    def __init__(self):
-        self.parent = parent()
-        self.rank, self.size = defaultdict(int), defaultdict(lambda:1)
-        
-    def find(self, x):
-        while self.parent[x] != x:
-            x = self.parent[x]
-
-        # while self.parent[original] != original:
-        #     original = self.parent[original]
-        #     self.parent[original] = x
-
-        return x
-
-    def union(self, x, y):
-        root_x = self.find(x)
-        root_y = self.find(y)
-
-        if root_x == root_y:
-            return
-
-        if self.rank[root_x] < self.rank[root_y]:
-            self.parent[root_x] = root_y
-            self.size[root_y] += self.size[root_x]
-        elif self.rank[root_x] > self.rank[root_y]:
-            self.parent[root_y] = root_x
-            self.size[root_x] += self.size[root_y]
-        else:
-            self.parent[root_y] = root_x
-            self.size[root_x] += self.size[root_y]
-            self.rank[root_x] += 1
-        
-    def connected(self, x, y):
-        return self.find(x) == self.find(y)   
-    
-    def count(self):
-        return len({self.find(x) for x in list(self.parent)})
-    
-    def setter(self, x, par):
-        self.parent[x] = par
-
 class Solution:
-    def friendRequests(self, n: int, restrictions: List[List[int]], requests: List[List[int]]) -> List[bool]:
-        
-        uf = UnionFind()
-        ans = [True]*len(requests)
-        idx = 0
-        for a, b in requests:
-            prev_a, prev_b = uf.find(a), uf.find(b)
-            uf.union(a, b)
-            
-            for i, j in restrictions:
-                if uf.connected(i, j):
-                    uf.setter(prev_a, prev_a)
-                    uf.setter(prev_b, prev_b)
-                    ans[idx] = False
-                    break
-            
-            idx += 1
-            
-        return ans
+    def friendRequests(self, n, restrictions, requests):
+        uf = {i: i for i in range(n)}
+        res = []
+
+        def find(i):
+            if i != uf[i]:
+                uf[i] = find(uf[i])
+            return uf[i]
+
+        for i, j in requests:
+            success = True
+            pi, pj = find(i), find(j)
+            if pi != pj:
+                for x, y in restrictions:
+                    px, py = find(x), find(y)
+                    if (px, py) == (pi, pj) or (px, py) == (pj, pi):
+                        success = False
+                        break
+            if success:
+                uf[pj] = pi
+            res.append(success)
+        return res
